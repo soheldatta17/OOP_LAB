0d226275b6b4643cb287e8628007872f11a00a0a
soheldatta17 <soheldatta17@gmail.com>
2024-07-30 08:21:23 +0530

    first commit

---
+++ P1.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+class Number {
+
+    int value;
+
+    public String isPrime() {
+        if (value <= 1) {
+            return "Not Prime";
+        }
+        if (value == 2) {
+            return "Prime";
+        }
+        if (value % 2 == 0) {
+            return "Not Prime";
+        }
+        for (int i = 3; i <= Math.sqrt(value); i += 2) {
+            if (value % i == 0) {
+                return "Not Prime";
+            }
+        }
+        return "Prime";
+    }
+
+    public String oddOrEven() {
+        return (value % 2 == 0) ? "Even" : "Odd";
+    }
+
+    public void setval(int value) {
+        this.value = value;
+    }
+
+    public int getval() {
+        return value;
+    }
+}
+
+public class P1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number: ");
+        int value = sc.nextInt();
+        
+        Number num = new Number();
+        num.setval(value);
+        
+        String primeStatus = num.isPrime();
+        String oddOrEven = num.oddOrEven();
+        
+        System.out.println("\n--- Number Analysis ---");
+        System.out.println("Number Entered    : " + num.getval());
+        System.out.println("Prime Status      : " + primeStatus);
+        System.out.println("Odd or Even       : " + oddOrEven);
+        System.out.println("------------------------");
+        
+        sc.close();
+    }
+}
---
+++ P2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class Year {
+
+    int year;
+
+    public String isLeapYear() {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ? "Leap Year" : "Not a Leap Year";
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
+
+public class P2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the year: ");
+        int year = sc.nextInt();
+        
+        Year yr = new Year();
+        yr.setYear(year);
+        
+        String leapYearStatus = yr.isLeapYear();
+        
+        System.out.println("\n--- Year Analysis ---");
+        System.out.println("Year Entered      : " + yr.getYear());
+        System.out.println("Leap Year Status  : " + leapYearStatus);
+        System.out.println("-----------------------");
+        
+        sc.close();
+    }
+}
